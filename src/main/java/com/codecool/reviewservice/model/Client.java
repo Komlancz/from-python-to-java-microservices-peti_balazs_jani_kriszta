98ba13c86e679844bfa2e8cddff68e40bba270c5
Komlancz Janos <jkomlancz@gmail.com>
2017-01-14 18:43:13 +0100

    Client doc done

--- src/main/java/com/codecool/reviewservice/model/Client.java
+++ src/main/java/com/codecool/reviewservice/model/Client.java
@@ -8,38 +8,67 @@ public class Client {
     String APIKey;
     String email;
 
+    /**
+     * This method has to generate an (UUID) APIKey if it did not got yet.
+     * @param name - not null.
+     * @param email - not null.
+     */
     public Client(String name, String email){
         this.name = name;
         this.email = email;
         APIKey = UUID.randomUUID().toString();
     }
 
+    /**
+     *
+     * @param name - not null.
+     * @param email - not null.
+     * @param APIKey - not null.
+     */
     public Client(String name, String email, String APIKey){
         this.name = name;
         this.email = email;
         this.APIKey = APIKey;
     }
 
+    /**
+     * @param id - not null.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * @return Name of the client
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return Client's id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @return Client's API key
+     */
     public String getAPIKey(){
         return APIKey;
     }
 
+    /**
+     * @return Client's e-mail address
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * @return Company name: name, Email address: email, API Key: APIKey
+     */
     @Override
     public String toString(){
         return String.format("" +
