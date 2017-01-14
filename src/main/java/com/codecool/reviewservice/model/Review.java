4c6a25534878c2841feafd8b462b2c1ebac9afab
Komlancz Janos <jkomlancz@gmail.com>
2017-01-14 13:45:59 +0100

    review model javadoc done

--- src/main/java/com/codecool/reviewservice/model/Review.java
+++ src/main/java/com/codecool/reviewservice/model/Review.java
@@ -17,6 +17,15 @@ public class Review {
     private String status;
     private String reviewKey;
 
+    /**
+     *
+     * @param clientID  - not null.
+     * @param productName - not null and it has to be uppercase without any whitespace.
+     * @param comment - not null.
+     * @param rating - not null.
+     */
+
+
 //    Constructor without reviewKey, it generates new
     public Review(int clientID, String productName, String comment, int rating) {
         this.clientID = clientID;
@@ -29,6 +38,15 @@ public class Review {
         logger.info("Create new Review model with new reviewKey: " + this.reviewKey + " Status: " + this.status);
     }
 
+    /**
+     *
+     * @param clientID - not null.
+     * @param productName - not null and it has to be uppercase without any whitespace.
+     * @param comment - not null.
+     * @param rating - not null.
+     * @param reviewKey - not null.
+     * @param status - not null.
+     */
 //    Constructor with reviewKey
     public Review(int clientID, String productName, String comment, int rating, String reviewKey, String status) {
         this.clientID = clientID;
@@ -41,53 +59,96 @@ public class Review {
         logger.info("Create Review model with the reviewKey: " + this.reviewKey + " Status: " + this.status);
 
     }
-
+    /**
+     *
+     * @return - the current status of the review
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * This method set the current review's status to Approved.
+     * @return - Nothing.
+     */
     public void setStatusApproved() {
         logger.info("Set status to: " + Status.APPROVED);
         this.status = Status.APPROVED.toString();
     }
 
+    /**
+     * This method set the current review's status to Denied.
+     * @return - Nothing.
+     */
     public void setStatusDenied() {
         logger.info("Set status to: " + Status.DENIED);
         this.status = Status.DENIED.toString();
     }
 
+    /**
+     * This method converts the product name to uppercase and deletes the spaces
+     * @param prodName - not null.
+     * @return . Uppercase version of prodName without space.
+     */
     private String handleProductName(String prodName){
         return prodName.replace(" ", "").toUpperCase();
     }
 
+    /**
+     *
+     * @return - name of the review's product.
+     */
     public String getProductName() {
         return productName;
     }
 
+    /**
+     * This method set the review model's id what gets from database.
+     * @param id not null.
+     */
     public void setId(String id){
         this.id = id;
     }
 
+    /**
+     * @return - current review's id.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * @return - Id of the client who sent the review.
+     */
     public int getClientID() {
         return clientID;
     }
 
+    /**
+     * @return - text of the comment.
+     */
     public String getComment() {
         return comment;
     }
 
+    /**
+     * @return - value of the rating.
+     */
     public int getRating() {
         return rating;
     }
 
+    /**
+     * @return - generated review key.
+     */
     public String getReviewKey() {
         return reviewKey;
     }
 
+    /**
+     * @return - Client_id: id, Product name: prodName, Review: comment's text, Ratings: value of rating,
+     * Review Key: review key, Status: status of review - in a string
+     */
     @Override
     public String toString(){
         return String.format(
