55db01b489b32e97a528e40aaa0a9871845da51e
Komlancz Janos <jkomlancz@gmail.com>
2017-01-15 19:08:14 +0100

    ClientDao and ReviewDao docs are already

--- src/main/java/com/codecool/reviewservice/dao/ClientDao.java
+++ src/main/java/com/codecool/reviewservice/dao/ClientDao.java
@@ -3,8 +3,26 @@ package com.codecool.reviewservice.dao;
 import com.codecool.reviewservice.model.Client;
 
 public interface ClientDao {
+    /**
+     * This method gets a client object, creates an sql INSERT query and uploads that to the database.
+     * @param clientModel not null.
+     */
     void add(Client clientModel);
+    /**
+     * This method gets an API key and deletes the client from the database where api key is same.
+     * @param APIKey not null.
+     */
     void remove(String APIKey);
+    /**
+     * This method gets an id search it in database and create a client object with details.
+     * @param id not null.
+     * @return Client object which has same id
+     */
     Client getById(int id);
+    /**
+     * This method gets an APIKey search it in database and create a client object with details.
+     * @param APIKey not null.
+     * @return Client object which has same APIKey
+     */
     Client getByAPIKey(String APIKey);
 }
--- src/main/java/com/codecool/reviewservice/dao/ReviewDao.java
+++ src/main/java/com/codecool/reviewservice/dao/ReviewDao.java
@@ -5,11 +5,44 @@ import com.codecool.reviewservice.model.Review;
 import java.util.ArrayList;
 
 public interface ReviewDao {
+    /**
+     * This method gets a review object, creates an sql INSERT query and uploads that to the database.
+     * @param reviewModel not null.
+     */
     void add(Review reviewModel);
-    void remove(int id);
+    /**
+     * This method gets an review key and deletes the review from the database where review key is same.
+     * @param reviewKey not null.
+     */
+    void remove(int reviewKey);
+    /**
+     * This method get a client id and search review(s) whose match
+     * @param clientID not null.
+     * @return List of review object(s).
+     */
     ArrayList<Review> getByClientID(int clientID);
+    /**
+     * This method gets a product name and searches review(s) whose match
+     * @param productName not null.
+     * @return Lis of review object(s).
+     */
     ArrayList<Review> getByProductName(String productName);
+    /**
+     * This method gets a client id and searches review(s) whose match and the status is APPROVED
+     * @param clientID not null.
+     * @return Lis of review object(s).
+     */
     ArrayList<Review> getApprovedByClientId(int clientID);
+    /**
+     * This method gets a product name and searches review(s) whose match and the status is APPROVED
+     * @param productName not null.
+     * @return Lis of review object(s).
+     */
     ArrayList<Review> getApprovedByProductName(String productName);
+    /**
+     * This method searches review by review key and updates status
+     * @param review_key not null.
+     * @param newStatus not null.
+     */
     void updateStatus(String review_key, String newStatus);
 }
