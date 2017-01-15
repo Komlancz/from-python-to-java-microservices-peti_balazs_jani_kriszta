7524cc2b4d2039b0106f551bc2148764e166f949
Komlancz Janos <jkomlancz@gmail.com>
2017-01-16 00:08:33 +0100

    ClientController doc is already

--- src/main/java/com/codecool/reviewservice/controller/ClientController.java
+++ src/main/java/com/codecool/reviewservice/controller/ClientController.java
@@ -14,9 +14,16 @@ public class ClientController {
     private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
     private static ClientDao clientDao = ClientDaoJdbc.getInstance();
 
-
-    // create new client & save it to the database & sending registration email
-    // TODO: 2017.01.04. integrate email sending service
+    /**
+     * This method has called from server (/review/:APIKey/:productName/:comment/:ratings).
+     * It instantiates a new Client object, saves it into the database and sends an email to the new client
+     * which contains the client's registration data and the API Key which they will need later
+     * for using the API service.
+     * At the end it redirects to the "/success" route.
+     * @param request   Spark request object
+     * @param response  Spark response object
+     * @return route: "/success"
+     */
     public static ModelAndView newClient(Request request, Response response) {
         Client newClient = new Client(request.queryParams("name"), request.queryParams("email"));
         logger.info("New client created: {}", newClient.toString());
