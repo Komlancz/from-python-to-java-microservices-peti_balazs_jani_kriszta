4b8f79a3b21791ed417fe1d8fbebf092d36f5511
Komlancz Janos <jkomlancz@gmail.com>
2017-01-16 00:48:56 +0100

    docs are already

--- src/main/java/com/codecool/reviewservice/Server.java
+++ src/main/java/com/codecool/reviewservice/Server.java
@@ -9,7 +9,51 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import java.sql.SQLException;
 
 import static spark.Spark.*;
-
+/**
+ * <h1>Horseshoe Review Service</h1>
+ * Horseshoe Review Service is a cross-store review management system.
+ * The service provides a powerful solution to create, moderate and display product reviews in
+ * your online store with an integrated possibility to display approved reviews from other online
+ * stores as well.
+ * <p>
+ * Clients need to register with Horseshoe Review Service by providing their company name and email address.
+ * After that, an email will be sent to them with their registration data and API Key, which is validated every time
+ * they wish to use the Review Service.
+ *
+ * Features:
+ *  1. Register new Company
+ *  2. Users of the web shops are able the submit reviews
+ *  3. Clients are able to update the status of the reviews (APPROVED or DENIED) submitted on their site with links
+ *  provided for them via email.
+ *  4. Clients are able to request all the reviews approved by them
+ *  5. Clients are able to request all approved reviews of a specific product from the entire database.
+ *
+ * @author  Jani Peti Balazs Kriszta
+ * @version 1.0
+ * @since   2017-01-06
+ *//**
+ * <h1>Horseshoe Review Service</h1>
+ * Horseshoe Review Service is a cross-store review management system.
+ * The service provides a powerful solution to create, moderate and display product reviews in
+ * your online store with an integrated possibility to display approved reviews from other online
+ * stores as well.
+ * <p>
+ * Clients need to register with Horseshoe Review Service by providing their company name and email address.
+ * After that, an email will be sent to them with their registration data and API Key, which is validated every time
+ * they wish to use the Review Service.
+ *
+ * Features:
+ *  1. Register new Company
+ *  2. Users of the web shops are able the submit reviews
+ *  3. Clients are able to update the status of the reviews (APPROVED or DENIED) submitted on their site with links
+ *  provided for them via email.
+ *  4. Clients are able to request all the reviews approved by them
+ *  5. Clients are able to request all approved reviews of a specific product from the entire database.
+ *
+ * @author  Jani Peti Balazs Kriszta
+ * @version 1.0
+ * @since   2017-01-06
+ */
 public class Server {
 
 
